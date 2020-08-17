88f8c65abf1e2b4597cec8d913216db549d34125
CleverSource <stunthound@gmail.com>
2020-08-17 01:39:45 -0400

    Add support for Linux

--- premake5.lua
+++ premake5.lua
@@ -18,9 +18,35 @@ project "GLFW"
 		"src/window.c"
 	}
 
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		staticruntime "On"
+
+		files
+		{
+			"src/x11_init.c",
+			"src/x11_monitor.c",
+			"src/x11_window.c",
+			"src/xkb_unicode.c",
+			"src/posix_time.c",
+			"src/posix_thread.c",
+			"src/glx_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c",
+			"src/linux_joystick.c"
+		}
+
+		defines
+		{
+			"_GLFW_X11"
+		}
+
 	filter "system:windows"
 		buildoptions { "-std=c11", "-lgdi32" }
-		systemversion "10.0.17134.0"
+
+		systemversion "latest"
 		staticruntime "On"
 
 		files
@@ -42,5 +68,10 @@ project "GLFW"
 			"_CRT_SECURE_NO_WARNINGS"
 		}
 
-	filter { "system:windows", "configurations:Release" }
-		buildoptions "/MT"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
